b51eb73166ae07a957806041e7a90bfe17034dd0
mhmmdadisptro <muhammadxxz7@gmail.com>
2023-05-21 20:43:07 +0700

    push migration & seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'nik' => '123456789',
+            'name' => 'Muhammad Adi Saputro',
+            'email'=>'muhammadxx7@gmail.com',
+            'phone'=>'08123',
+            'password'=>bcrypt('123')
+        ]);
     }
 }
