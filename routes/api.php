f49e49d2189aa58824aa4d0429a8225a65a1da4c
mhmmdadisptro <muhammadxxz7@gmail.com>
2023-05-21 20:39:47 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+/**
+ * Login
+ * 
+ * - phone
+ * - password
+ */
 Route::post('login',[AuthController::class, 'login']);
